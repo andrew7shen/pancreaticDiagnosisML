ed05b982aa349e6991d1e04da1f15da593583e50
Andrew Shen <43887284+andrew7shen@users.noreply.github.com>
2022-09-07 16:37:25 -0700

    Created function structures for project

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
 # Andrew Shen, 08/23/2022
 
+def read_in(input_file):
+    """
+    Reads in input data
+    :param input_file:
+    :return:
+    """
+
+
+def develop_model(training_data):
+    """
+    Develop machine learning model using training data fraction of dataset
+    :param training_data:
+    :return:
+    """
+
+
+def apply_model(testing_data, model):
+    """
+    Apply machine learning model to the testing data fraction of dataset
+    :param testing_data:
+    :param model:
+    :return:
+    """
+
+
+def test_model():
+    """
+    Verify validity of model
+    :return:
+    """
+
 
 if __name__ == '__main__':
     print("Main function running...")
